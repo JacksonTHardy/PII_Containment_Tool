b531c377b077e6f638d897cb2400d14fd1e1044a
JacksonTHardy <101376094+JacksonTHardy@users.noreply.github.com>
2022-07-13 08:48:16 -0400

    Add files via upload

--- PIIToolV1.py
+++ PIIToolV1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 # Import the os module
-from ast import pattern
-from operator import truediv
 import os
 from os import listdir
 from os.path import isfile, isdir, join
@@ -10,67 +8,59 @@ from pathlib import Path
 import re
 
 
-def getfiles(directory, filelist):
+def getfiles(directory: str, filelist: list[str]) -> list[str]:
     # print("Current working directory: {0}".format(directory))
 
-    for file in listdir(directory): # for each file in the listdir
-        path = join(directory, file) # concats directory with the file
-        if isdir(path): # determines if path is a exsisting directory or not
+    for file in listdir(directory):  # for each file in the listdir
+        path = join(directory, file)  # concats directory with the file
+        if isdir(path):  # determines if path is a exsisting directory or not
             filelist = getfiles(path, filelist)
-        elif isfile(path): # determines if path exist as a file
-            filelist.append(path) # if file exist it adds it to a list of all files
+        elif isfile(path):  # determines if path exist as a file
+            filelist.append(path)  # if file exist it adds it to a list of all files
         else:
             print("Error with current directory: {0}".format(path))
 
-    return filelist # returns a list of all the file names that exist
+    return filelist  # returns a list of all the file names that exist
 
 
-def processfiles(completefilelist):
-    fileText = ""
-    hasSSN = False
-    SSN = "ssn"
-    SocialSecurityNumber = "social security number"
-    NationalID = "national id"
-    #social = re.compile("\d{3}-\d{2}-\d{4}")
+def processfiles(completefilelist: list):
     for file in completefilelist:
-        ext = Path(file).suffix # saves the file suffix like .txt into ext
-        #print(ext)
+        ext: str = Path(file).suffix  # saves the file suffix like .txt into ext
         match ext:
-            case ".txt" | ".csv" | ".json" | ".xml":
-                with open(file, "r") as f:
-                    #printFileContents(file)
-                    for line in f:
-                        if SSN in line.lower():
-                            hasSSN = True
-                        if SocialSecurityNumber in line.lower():
-                            hasSSN = True
-                        if NationalID in line.lower():
-                            hasSSN = True
-
-                    f.seek(0)
-                    socialFound = re.search("(\d{3}-\d{2}-\d{4})", f.read())
-                    if socialFound != None:
-                        print(file)
-                    if hasSSN == True:
-                        print(file)
-                        hasSSN = False
-                #scanfile(file, text)
-                f.close
+            case ".txt" | ".csv" | ".json" | ".xml":  # if the file ends in one of the following extensions
+                scanfile(file)
 
 
-#def scanfile(file, text):
- #   with open(file, "r") as f:
-  #      if text in f.read(): # if text is in the file f
-   #         print(file)
+def scanfile(file: str):
+    with open(file, "r") as f:
+        SSN: str = "ssn"
+        social_security_number: str = "social security number"
+        national_id: str = "national id"
+        has_ssn: bool = False
+        for line in f:  # for each line in the file
+            if (
+                SSN and social_security_number and national_id in line.lower()
+            ):  # check for hard coded strings in the file
+                has_ssn = True
 
-def printFileContents(fileToPrint):
-    with open(fileToPrint, "r") as prtFile:
-        print(prtFile.readlines())
+        f.seek(0)  # reset the file position back to the beginning
+        social_found = re.search(
+            "(\\d{3}-\\d{2}-\\d{4})", f.read()
+        )  # search for this specific pattern
+        if (
+            social_found is not None or has_ssn is True
+        ):  # if file is flagged print the file name
+            print(file)
+            has_ssn = False
+        f.close
+
 
 def main():
-    cwd = os.getcwd() # get the current working directory
-    filelist = [] # creates array
-    completefilelist = getfiles(cwd, filelist) # fills filelist woth all the files in the current working directory
+    cwd = os.getcwd()  # get the current working directory
+    filelist = []  # creates array
+    completefilelist = getfiles(
+        cwd, filelist
+    )  # fills filelist woth all the files in the current working directory
 
     print("Files with matching text:")
     processfiles(completefilelist)
