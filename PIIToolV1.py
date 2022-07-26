1dca6a8cf0245b04fcbf329af270b406a961a964
Alex Will <Alex.Will.04342@gmail.com>
2022-07-26 13:30:14 -0400

    Implemented File Class

--- PIIToolV1.py
+++ PIIToolV1.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# Import the os module
-# import os
-# from ctypes import sizeof
-
 import logging
 import re
 import subprocess
@@ -13,70 +9,59 @@ from os import listdir
 from os.path import isdir, isfile, join, exists
 from pathlib import Path
 from tkinter import filedialog
+from file_class import File
 from severity_enum import Severity
 
 
-def getfiles(directory: str, filelist: list[str]) -> list[str]:
+def getfiles(directory: str, file_folder: list[File]) -> list[File]:
     # print("Current working directory: {0}".format(directory))
-    for file in listdir(directory):  # for each file in the listdir
-        path: str = join(directory, file)  # concat directory with the file
+    for item in listdir(directory):  # for each file in the listdir
+        path: str = join(directory, item)  # concat directory with the file
         if isdir(path):  # determines if path is a existing directory or not
-            getfiles(path, filelist)
+            getfiles(path, file_folder)
         elif isfile(path):  # determines if path exist as a file
-            filelist.append(path)  # if file exist it adds it to a list of all files
+            file: File = File(item, directory)
+            file_folder.append(file)  # if file exist it adds it to a list of all files
         else:
             print("Error with current directory: {0}".format(path))
 
-    return filelist  # returns a list of all the file names that exist
+    return file_folder  # returns a list of all the file names that exist
 
 
-def processfiles(completefilelist: list[str]) -> None:
-    """_summary_
-    Next: store files, prompt user to open flagged files.
-    Args:
-        completefilelist (list): _description_
-    """
-    for file in completefilelist:
-        ext: str = Path(file).suffix  # saves the file suffix like .txt into ext
-        match ext:
+def processfiles(file_folder: list[File]) -> None:
+
+    for file in file_folder:
+        # saves the file suffix like .txt into ext
+        file.extension = Path(file.filepath).suffix
+        match file.extension:
             # if the file ends in one of the following extensions
             case ".txt" | ".csv" | ".json" | ".xml":
                 scanfile(file)
             case _:
                 logging.info(
-                    f"This file type is not currently handled| Ext: {ext}| File: {file}"
+                    f"This file type is not currently handled| Ext: {file.extension}| File: {file.filepath}"
                 )
 
 
-def scanfile(file: str) -> None:
-    with open(file, "r") as f:
+def scanfile(file: File) -> None:
+    with open(file.filepath, "r") as f:
         file_contents: str = f.read()
-        if re.search(
-            "national[\\s_]?id|social[\\s_]?security[\\s_]?number|ssn|(\\d{3}-\\d{2}-\\d{4})",
+        if re.search("(\\d{3}-\\d{2}-\\d{4})", file_contents):
+            file.severity = Severity.red
+        elif re.search(
+            "national[\\s_]?id|social[\\s_]?security[\\s_]?number|ssn",
             file_contents.lower(),
         ):
-            storefile(file)
-            process_severity(file_contents)
+            file.severity = Severity.yellow
+        else:
+            file.severity = Severity.green
 
 
-def displayfiles() -> None:
-    for file in flagged_files:
-        print(file)
-
-
-def storefile(file: str) -> None:
-    flagged_files.append(file)
-
-
-def process_severity(contents: str) -> None:
-    if re.search("(\\d{3}-\\d{2}-\\d{4})", contents):
-        flagged_severity.append(Severity.red)
-    elif re.search(
-        "national[\\s_]?id|social[\\s_]?security[\\s_]?number|ssn", contents.lower()
-    ):
-        flagged_severity.append(Severity.yellow)
-    else:
-        flagged_severity.append(Severity.green)
+def displayfiles(file_folder: list[File]) -> None:
+    file_folder.sort(key=lambda x: x.severity.value, reverse=True)
+    for file in file_folder:
+        if file.severity.value > 0:
+            print(f"{file.filename}: {file.severity.name}")
 
 
 def find_next_path(filepath: str) -> str:
@@ -95,7 +80,7 @@ def create_file_data(filepath: str) -> None:
         find_next_path(filepath + "flaggedFileData"),
         "w",
     ) as f:
-        for file in flagged_files:
+        for file in file_folder:
             f.write(file)
             f.write(flagged_severity[counter].__str__() + "\n")
             counter += 1
@@ -103,20 +88,18 @@ def create_file_data(filepath: str) -> None:
 
 
 def main() -> None:
-    filelist: list[str] = []  # creates array
+    file_folder: list[File] = []  # creates array
     tkinter.Tk(screenName="PII Containment Tool").withdraw()
     path: str = filedialog.askdirectory(title="PII Containment Tool")
-    completefilelist: list[str] = getfiles(
-        path, filelist
-    )  # fills filelist with all the files in the current working directory
-
-    processfiles(completefilelist)
-    if len(flagged_files) != 0:
-        print(f"Files Flagged | File Count: {len(flagged_files)}")
-        displayfiles()
-        print("\n")
-        print(flagged_severity)
-        create_file_data("Flagged_file_data_Log")
+    # fills filelist with all the files in the current working directory
+    getfiles(path, file_folder)
+    processfiles(file_folder)
+    if len([x for x in file_folder if x.severity.value > 0]) != 0:
+        print(
+            f"Files Flagged | File Count: {len([x for x in file_folder if x.severity.value>0])}"
+        )
+        displayfiles(file_folder)
+        create_file_data("Flagged_file_data_Log\\")
 
         while True:
             open_files: str = input("Would you like to open these flagged files? (Y/N)")
@@ -126,16 +109,15 @@ def main() -> None:
                 print("That Command is not Recognized")
 
         if open_files.upper() == "Y":
-            for file in flagged_files:
-                subprocess.Popen(["notepad.exe", file])
+            for file in file_folder:
+                if file.severity.value > 0:
+                    subprocess.Popen(["notepad.exe", file.filepath])
     else:
         print("No Flagged files")
 
 
 if __name__ == "__main__":
-    global flagged_files
-    flagged_files: list[str] = []
-    global flagged_severity
+    file_folder: list[str] = []
     flagged_severity: list[object] = []
     now: str = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
     logging.basicConfig(
